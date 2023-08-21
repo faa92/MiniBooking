23ed36145ab28c3393c25152242c4c97da5d48d7
AntonFedoseev <fedoseev446@gmail.com>
2023-08-21 22:01:12 +0300

    Initial Commit

---
+++ src/test/java/com/example/minibooking/MiniBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minibooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
