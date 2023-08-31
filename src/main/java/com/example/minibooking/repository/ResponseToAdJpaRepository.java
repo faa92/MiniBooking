4c218e5e03b003fcdfa6a699361b73bb86401688
AntonFedoseev <fedoseev446@gmail.com>
2023-08-31 20:36:24 +0300

    add ResponseToAdJpaRepository

---
+++ src/main/java/com/example/minibooking/repository/ResponseToAdJpaRepository.java
@@ -0,0 +1,54 @@
+package com.example.minibooking.repository;
+
+import com.example.minibooking.model.responseToAd.ResponseToAd;
+
+import java.util.List;
+
+public class ResponseToAdJpaRepository extends BaseJpaRepository<ResponseToAd, Long> implements ResponseToAdRepository {
+    public ResponseToAdJpaRepository() {
+        super(ResponseToAd.class);
+    }
+
+    @Override
+    public List<ResponseToAd> findPageByTenant(long tenantId, int pageSize, int pageNumber) {
+        return entityManager.createQuery("""
+                        SELECT responsetoad
+                        FROM ResponseToAd responseToAd
+                        WHERE responseToAd.tenant.id = :tenantId
+                        ORDER BY responseToAd.createdAt DESC 
+                        """, ResponseToAd.class)
+                .setParameter("tenantId", tenantId)
+                .setMaxResults(pageSize)
+                .setFirstResult(pageSize * pageNumber)
+                .getResultList();
+    }
+
+    @Override
+    public List<ResponseToAd> findByRentalAdLandlordAndTenant(long landlordId, long tenantId, int pageSize, int pageNumber) {
+        return entityManager.createQuery("""
+                        SELECT responsetoad
+                        FROM ResponseToAd responseToAd
+                        WHERE responseToAd.rentalAd.landlord.id = :landlordId
+                        AND responseToAd.tenant.id = :tenantId
+                        ORDER BY responseToAd.createdAt DESC 
+                        """, ResponseToAd.class)
+                .setParameter("landlordId", landlordId)
+                .setParameter("tenantId", tenantId)
+                .setMaxResults(pageSize)
+                .setFirstResult(pageSize * pageNumber)
+                .getResultList();
+    }
+
+    @Override
+    public int countResponseToAdByLandlord(long landlordId) {
+        Integer count = entityManager.createQuery("""
+                        SELECT COUNT (responsetoad)
+                        FROM ResponseToAd responseToAd
+                        WHERE responseToAd.rentalAd.landlord.id = :landlordId
+                        """, Integer.class)
+                .setParameter("landlordId", landlordId)
+                .getSingleResult();
+
+        return count.intValue();
+    }
+}
--- src/main/java/com/example/minibooking/repository/ResponseToAdRepository.java
+++ src/main/java/com/example/minibooking/repository/ResponseToAdRepository.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 public interface ResponseToAdRepository extends BaseRepository<ResponseToAd, Long> {
 
-    List<ResponseToAd> findAllByTenant(long tenantId, int pageSize, int pageNumber);
+    List<ResponseToAd> findPageByTenant(long tenantId, int pageSize, int pageNumber);
 
-    List<ResponseToAd> findAllByRentalAd(long rentalAdId, int pageSize, int pageNumber);
+    List<ResponseToAd> findByRentalAdLandlordAndTenant(long landlordId, long tenantId, int pageSize, int pageNumber);
 
-    int countByRentalAdLandlord(long landlordId);
+    int countResponseToAdByLandlord(long rentalAdId);
 
 }
